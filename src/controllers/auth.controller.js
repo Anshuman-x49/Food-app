c72e1cde1a24d0b0ffaa955dfa01fa148b98d284
Anshuman-x49 <anshumannayak169@gmail.com>
2026-06-02 19:59:11 +0530

    implement logout feature and fix some bugs

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,166 +1,207 @@
-import usermodel from "./models/user.model.js"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcryptjs"
-import { jwt_secret } from "../config/config.js"
-
+import usermodel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { jwt_secret } from "../config/config.js";
 
 /**
- * 
+ *
  * @desc register a new user
  * @route POST /api/v1/auth/register
  * @access public
  */
 const registerUser = async (req, res) => {
-    try {
-        const { username, email, password, role } = req.body
+  try {
+    const { username, email, password, role } = req.body;
 
-        if(!username || !email || !password, !role) {
-            return res.status(400).json({
-                success: false,
-                message: "Please provide all the required fields"
-            })
-        }
-
-        const userExist = await usermodel.findOne({
-            $or: [
-                {email},
-                {username}
-            ]
-        })
-
-        if(userExist){
-            return res.status(400).json({
-                success: false,
-                message: "User already exists"
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10)
-
-        const user = await usermodel.create({
-            username,
-            email,
-            password: hashedPassword,
-            role
-        })
-
-        const refreshToken = await jwt.sign({
-            id: user._id
-        },jwt_secret,{
-            expiresIn: "7d"
-        })
-
-        const accessToken = await jwt.sign({
-            id: user._id
-        }, jwt_secret, {
-            expiresIn: "10m"
-        })
-
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-        })
-
-        return res.status(201).json({
-            message: "User registered successfully",
-            user: {
-                id: user._id,
-                username,
-                email,
-                role,
-                accessToken
-            }
-        })
-    } catch (error) {
-        console.log("Error in user registration:", error)
-        return res.status(500).json({
-          success: false,
-          message: "An error occurred while registering the user. Please try again."
-        })
+    if (!username || !email || !password || !role) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide all the required fields",
+      });
     }
-}
+
+    const userExist = await usermodel.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (userExist) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await usermodel.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    const refreshToken = await jwt.sign(
+      {
+        id: user._id,
+      },
+      jwt_secret,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    const accessToken = await jwt.sign(
+      {
+        id: user._id,
+      },
+      jwt_secret,
+      {
+        expiresIn: "10m",
+      },
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        username,
+        email,
+        role,
+        accessToken,
+      },
+    });
+  } catch (error) {
+    console.log("Error in user registration:", error);
+    return res.status(500).json({
+      success: false,
+      message:
+        "An error occurred while registering the user. Please try again.",
+    });
+  }
+};
 
 /**
- * 
+ *
  * @desc login an existing user
  * @route POST /api/v1/auth/login
  * @access public
  */
 const loginUser = async (req, res) => {
-    try {
-        const { username, email, password } = req.body
+  try {
+    const { username, email, password } = req.body;
 
-        if(!username || !email || !password) {
-            return res.status(400).json({
-                success: false,
-                message: "Please provide all the required fields"
-            })
-        }
-
-        const user = await usermodel.findOne({
-            $or: [
-                {username},
-                {email}
-            ]
-        })
-
-        if(!user){
-            return res.status(404).json({
-                success: false,
-                message: "User not found"
-            })
-        }
-
-        const ispasswordvalid = await bcrypt.compare(password, user.password)
-
-        if(!ispasswordvalid){
-            return res.status(401).json({
-                success: false,
-                message: "Invalid credentials"
-            })
-        }
-
-        const refreshToken = await jwt.sign({
-            id: user._id
-        }, jwt_secret, {
-            expiresIn: "7d"
-        })
-
-        const accessToken = await jwt.sign({
-            id: user._id
-        }, jwt_secret, {
-            expiresIn: "10m"
-        })
-
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-        })
-
-        return res.status(200).json({
-            message: "User logged in successfully",
-            user: {
-                id: user._id,
-                username,
-                email,
-                role,
-                accessToken
-            }
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            success: false,
-            message: "An error occurred while logging in the user. Please try again."
-        })
+    if ((!username && !email) || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide email or username, along with the password",
+      });
     }
-}
 
-module.export = {
-    registerUser,
-    loginUser
-}
+    const user = await usermodel.findOne({
+      $or: [
+        { username },
+        { email }
+      ],
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const ispasswordvalid = await bcrypt.compare(password, user.password);
+
+    if (!ispasswordvalid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const refreshToken = await jwt.sign(
+      {
+        id: user._id,
+      },
+      jwt_secret,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    const accessToken = await jwt.sign(
+      {
+        id: user._id,
+      },
+      jwt_secret,
+      {
+        expiresIn: "10m",
+      },
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        accessToken,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while logging in the user. Please try again.",
+    });
+  }
+};
+
+/**
+ *
+ * @desc logout an existing user
+ * @route POST /api/v1/auth/logout
+ * @access public
+ */
+const logoutUser = async (req, res) => {
+  try {
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.log("Error in user logout:", error);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while logging out. Please try again.",
+    });
+  }
+};
+
+export default {
+  registerUser,
+  loginUser,
+  logoutUser,
+};
